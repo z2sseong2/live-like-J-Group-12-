511642d02d7505d5a50a4fa2118d5b7bc1e70110
https://github.com/z2sseong2/live-like-J-Group-12-.git <leesoomin059@naver.com>
2024-11-28 20:11:19 +0900

    feat: node main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000
-app.use(express.static('public'));
+
+app.use(express.static('login'));
+app.use(express.static('images'));
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
