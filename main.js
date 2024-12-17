86aa54d24719ae40bb7d7d6d600d13db3eab7e5a
mintWaterMelon <134702226+lee990324@users.noreply.github.com>
2024-12-17 16:26:05 +0900

    Modfied

--- main.js
+++ main.js
@@ -3,35 +3,8 @@ const app = express();
 const port = 3000
 
 app.use(express.static('1_Main'));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/1_Main/login.html');
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/1_Main/sign_up.html');
-});
-
 app.use(express.static('2_Hub'));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/2_Hub/calendar.html');
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/2_Hub/inputschedule.html');
-});
-
 app.use(express.static('3_Yearly-Calendar'));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/3_Yearly-Calendar/imsi.html');
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/3_Yearly-Calendar/yearlycalendar.html');
-});
-
 app.use(express.static('4_My'));
 app.use(express.static('5_Monthly-CM'));
 app.use(express.static('6_Memo'));
@@ -45,6 +18,47 @@ app.use(express.static('13_Yearly-CM'));
 app.use(express.static('14_Weekly-CM'));
 app.use(express.static('15_Daily-CM'));
 app.use(express.static('16_Guideline'));
+app.use(express.static('images'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/1_Main/login.html');
+});
+
+app.get('/sign_up', (req, res) => {
+    res.sendFile(__dirname + '/1_Main/sign_up.html');
+});
+
+app.get('/calendar', (req, res) => {
+    res.sendFile(__dirname + '/2_Hub/calendar.html');
+});
+
+app.get('/inputschedule', (req, res) => {
+    res.sendFile(__dirname + '/2_Hub/inputschedule.html');
+});
+
+app.get('/imsi', (req, res) => {
+    res.sendFile(__dirname + '/3_Yearly-Calendar/imsi.html');
+});
+
+app.get('/yearlycalendar', (req, res) => {
+    res.sendFile(__dirname + '/3_Yearly-Calendar/yearlycalendar.html');
+});
+
+app.get('/mypage', (req, res) => {
+    res.sendFile(__dirname + '/4_My/mypage.html');
+});
+
+app.get('/monthlycm', (req, res) => {
+    res.sendFile(__dirname + '/5_Monthly-CM/monthlycm.html');
+});
+
+app.get('/memo', (req, res) => {
+    res.sendFile(__dirname + '/6_Memo/memo.html');
+});
+
+app.get('/settings', (req, res) => {
+    res.sendFile(__dirname + '/7_Settings/settings.html');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
